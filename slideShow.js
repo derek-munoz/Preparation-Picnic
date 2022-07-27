605c581eeb6ea13bbb17cc5f9c3e05818d513957
derek-munoz <derek.ali.munoz@gmail.com>
2022-07-27 14:41:33 -0500

    work

---
+++ slideShow.js
@@ -0,0 +1,22 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].getElementsByClassName.display = "none";
+    }
+    slides[slideIndex-1].getElementsByClassName.display = "block";
+    
+}
